15a8347613cad1580adc642a1b571e26fe6e7d0b
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-01-10 10:02:56 -0400

    Initial commit

---
+++ Assets/Scripts/triggerscript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class triggerscript : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timeline.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
