9ab05c1b3c6b7b1b94b062c8045c24d4fd45aa18
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-01-10 11:00:02 -0400

    Finished Animation

--- Assets/Scripts/triggerscript.cs
+++ Assets/Scripts/triggerscript.cs
@@ -6,10 +6,16 @@ using UnityEngine.Playables;
 public class triggerscript : MonoBehaviour
 {
     public PlayableDirector timeline;
+    public bool playOnce;
 
     private void OnTriggerEnter(Collider other)
     {
         timeline.Play();
+
+        if (playOnce == true)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerExit(Collider other)
